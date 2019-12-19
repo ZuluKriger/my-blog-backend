92015e05f71d6bc8d61b5605af33a80787c44122
RuneMadsen <webudvikler2020@gmail.com>
2019-12-19 13:39:53 +0100

    backend

---
+++ src/build/precache-manifest.a40dcf4a1c106bb8d67d23d893b619bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc785f46df2194bbb03e7ac50aa639f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc4a1e5b3d3514711c07",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "dcd71ea3c6573ba6be4f",
+    "url": "/static/js/2.f8401d34.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f8401d34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc4a1e5b3d3514711c07",
+    "url": "/static/js/main.24d7dbda.chunk.js"
+  },
+  {
+    "revision": "e738b54ffe3b15816cf3",
+    "url": "/static/js/runtime-main.63c1efab.js"
+  }
+]);
